3830a4c0f6b96e7bb289ec0720dde1c73e0fb56f
MyWsl <weicunzhe@qq.com>
2024-10-07 20:45:59 +0800

    修改服务器启动方式 和过滤bin目录

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -4,26 +4,31 @@
 #include <signal.h>
 using namespace std;
 
-
-//处理服务器ctrl+c结束后，重置user的状态信息
+// 处理服务器ctrl+c结束后，重置user的状态信息
 void resetHandler(int)
 {
     ChatService::instance()->reset();
     exit(0);
 }
 
-int main()
+int main(int argc, char **argv)
 {
-    signal(SIGINT,resetHandler);
 
+    if (argc < 3)
+    {
+        std::cerr << "command invalid example: ./ChatServer 127.0.0.1 6000" << std::endl;
+    }
+    char *ip = argv[1];
+    uint16_t port = atoi(argv[2]);
+
+    signal(SIGINT, resetHandler);
 
     muduo::net::EventLoop loop;
-    muduo::net::InetAddress addr("127.0.0.1", 6000);
+    muduo::net::InetAddress addr(ip, port);
     ChatServer server(&loop, addr, "ChatServer");
-    
+
     server.start();
     loop.loop();
-
 
     return 0;
 }
